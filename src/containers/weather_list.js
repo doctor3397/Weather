7e41483823f741b35802d239487895819f3a2511
Theresa Shen <tshen@architech.ca>
2017-11-11 16:20:59 -0500

    Use Sparklines and SparklinesLine to render temperature in a chart

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine} from 'react-sparklines';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    // console.log(temps);
+
     return (
-      <tr key={cityData.city.name}>
-        <td>{cityData.city.name}</td>
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Sparklines data={temps} width={180} height={120}>
+            <SparklinesLine color="blue" />
+          </Sparklines>
+        </td>
       </tr>
     );
   }
